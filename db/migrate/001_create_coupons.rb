0661b09c1d79963a116f7abf9452b89db28686a1
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-09 18:30:08 +0000

    automatically backed up by learn

--- db/migrate/001_create_coupons.rb
+++ db/migrate/001_create_coupons.rb
@@ -2,7 +2,7 @@ class CreateCoupons < ActiveRecord::Migration
   def change
     create_table :coupons do |t|
       t.string :coupon_code, null: false
-      t.string :store, null: false 
+      t.string :store, null: false
       
       t.timestamps null: false
     end
